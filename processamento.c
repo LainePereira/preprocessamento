21d7f790ecda779a496b3fd9c0bfde2a53cdeb67
LainePereira <144857991+LainePereira@users.noreply.github.com>
2023-11-15 16:46:49 -0300

    Update processamento.c
    codigo de teste para atividade de aula

--- processamento.c
+++ processamento.c
@@ -1,8 +1,18 @@
-#include <cs50.h>
-#include <stdio.h>
+#include "cs50.h"
+#include "stdio.h"
 
-int main(void)
-{
-    string nome = get_string("Qual o seu nome?");
-    printf("oi, %s\n", nome;
+
+int main() {
+    // Exemplo: Verificar se 7 é maior que 2
+    int resposta;
+
+    if (5 > 2) {
+        resposta = 1;
+    } else {
+        resposta = 0;
+    }
+
+    // Imprime a resposta
+    printf("Resposta: %d\n", resposta);
+    return 0;
 }
